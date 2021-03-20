f4624647940e033504f34775f1f8001b6c0046fa
Hugo Lageneste <hugo.lageneste@pm.me>
2021-03-20 09:35:45 +0100

    Install chakra ui for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { ChakraProvider, Text } from '@chakra-ui/react'
 
 const App = () => {
-  return <>
-    Hello
-  </>
+  return <ChakraProvider>
+    <Text>sds</Text>
+  </ChakraProvider>
 }
 
-export default App
+export default App;
