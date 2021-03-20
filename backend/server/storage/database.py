64486106fce29a885afb8cc737a1562f7e5d29ed
Thomas Marchand <thomas.marchand@tuta.io>
2021-03-20 07:07:49 +0100

    introduce SavedDict

--- backend/server/storage/database.py
+++ backend/server/storage/database.py
@@ -1,36 +1,48 @@
 import os
 import json
-import collections
+import collections.abc
 
-collections_abc = getattr(collections, 'abc', collections)
 
-class SavedList(collections_abc.MutableSequence):
-    def __init__(self, file_name, l=[]):
-        if type(l) is not list:
-            raise ValueError()
+class SavedDict(collections.abc.MutableMapping):
+    def __init__(self, file_name):
         self.file = get_path(file_name)
-        self._inner_list = l
+
+        if not os.path.isfile(self.file):
+            self.store = dict()
+        else:
+            with open(self.file, "r") as json_file:
+                self.store = json.load(json_file)
+                if type(self.store) is not dict:
+                    raise ValueError()
+
+    def write(self):
+        with open(self.file, "w") as outfile:
+            json.dump(self.store, outfile)
+
+    def __getitem__(self, key):
+        return self.store[self._keytransform(key)]
+
+    def __setitem__(self, key, value):
+        self.store[self._keytransform(key)] = value
+        self.write()
+
+    def __delitem__(self, key):
+        del self.store[self._keytransform(key)]
+        self.write()
+
+    def __iter__(self):
+        return iter(self.store)
 
     def __len__(self):
-        return len(self._inner_list)
+        return len(self.store)
 
-    def __delitem__(self, index):
-        self._inner_list.__delitem__(index - 1)
-
-    def insert(self, index, value):
-        self._inner_list.insert(index - 1, value)
-
-    def __setitem__(self, index, value):
-        self._inner_list.__setitem__(index - 1, value)
-
-    def __getitem__(self, index):
-        return self._inner_list.__getitem__(index - 1)
+    def _keytransform(self, key):
+        return key
 
 
 def get_path(name):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
 
-users = SavedList("files/users.json")
-users.append({5})
 
-print(users)
+users = SavedDict("files/users.json")
+users["thomas"] = {"id" : 12937462672}
