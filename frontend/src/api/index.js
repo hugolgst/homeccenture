194af8e544e7b5026a51f0fdbcd03486ba806e3b
Hugo Lageneste <hugo.lageneste@pm.me>
2021-03-20 18:48:58 +0100

    Add logos and change the API URL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = 'http://localhost:8000'
 
 export const registerUser = async (data) => {
     return await fetch(`${API_URL}/register`, {
